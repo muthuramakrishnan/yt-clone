be5f43bdacf5be72cffbc9d849632feeffabef6f
Muthuramakrishnan Mahadevan <muthu.rama.krishnan96@gmail.com>
2021-04-02 20:23:51 +0530

    ready for deployment

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3',
     params: {
-        key: process.env.REACT_APP_YOUTUBE_API_KEY
+        key: 'AIzaSyCgvjEIef8Dc6IHaew-wmxlf8bg9P2g8OQ'
     }
 });
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,6 @@ import firebase from 'firebase/app';
 
 import 'firebase/auth';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB0WaG8QhktU6NpvI9xLdpjAPmDbyvF89o",
-//     authDomain: "clone-1e430.firebaseapp.com",
-//     projectId: "clone-1e430",
-//     storageBucket: "clone-1e430.appspot.com",
-//     messagingSenderId: "147479076271",
-//     appId: "1:147479076271:web:b5d53dde3a39e41db7a35a"
-//   };
-
   const firebaseConfig = {
     apiKey: "AIzaSyCgvjEIef8Dc6IHaew-wmxlf8bg9P2g8OQ",
     authDomain: "clone-46af5.firebaseapp.com",
